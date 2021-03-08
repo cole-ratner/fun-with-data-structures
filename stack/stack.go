57c5d50fdc180b58709eae584ba00283d7b25086
Cole Ratner <kolamiti92@gmail.com>
2021-03-08 16:56:25 +0200

    added stack

---
+++ stack/stack.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+
+// stack holds a slice
+type stack struct {
+	items []int
+}
+
+// push adds an item to the stack
+func (s *stack) push(i int) {
+	fmt.Printf("Adding: %d\n", i)
+	s.items = append(s.items, i)
+}
+
+// removes the last item from the stack
+func (s *stack) pop() int {
+	last := len(s.items) - 1
+	remove := s.items[last]
+	s.items = s.items[:last]
+
+	fmt.Printf("Removed %d from the stack\n", remove)
+	return remove
+}
+
+
+
+func main() {
+	s := stack{}
+
+	// adding items to the stack
+	fmt.Println("Adding new items to the stack:")
+	s.push(1)
+	s.push(2)
+	s.push(3)
+	///
+
+	// printing current stack
+	fmt.Printf("Current stack: %v\n\n", s)
+
+	// removing the last item from the stack
+	s.pop()
+	
+	//printing current stack
+	fmt.Printf("Current stack: %v\n", s)
+}
